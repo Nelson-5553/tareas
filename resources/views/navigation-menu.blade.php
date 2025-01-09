793c9b9f7b82317a8269d09d0075d0fd807e351a
Nelson-5553 <andresnelson2004@gmail.com>
2025-01-08 23:23:19 -0500

    fix error navigation var

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -26,7 +26,7 @@
                 </div>
 
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link href="{{ route('category') }}" :active="request()->routeIs('categorias')">
+                    <x-nav-link href="{{ route('category') }}" :active="request()->routeIs('category')">
                         Categorias
                     </x-nav-link>
 
