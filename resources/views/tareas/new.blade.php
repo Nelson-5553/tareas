32a8682d06386a7a3eb30c8742f6c52fce30ded3
Nelson-5553 <andresnelson2004@gmail.com>
2025-01-18 19:51:35 -0500

    add modal component

--- resources/views/tareas/new.blade.php
+++ resources/views/tareas/new.blade.php
@@ -66,10 +66,11 @@
                 @foreach ($tareas as $tarea)
                 <div class="w-64 h-64 bg-white rounded-lg border-2 border-black shadow-lg p-4 flex flex-col justify-between">
                     <!-- Header -->
-                    <div class="flex items-center space-x-2">
+
+                        <div class="flex flex-row justify-start items-center space-x-2">
                         <span class="inline-block w-6 h-6 rounded-full" style="background-color: {{ $tarea->Category->color ?? '#ccc' }};"></span>
                         <h1 class="font-bold text-lg">{{$tarea->name}}</h1>
-                    </div>
+                        </div>
 
                     <!-- Description -->
 
@@ -78,38 +79,23 @@
                     </p>
 
                     <!-- Footer (Buttons) -->
-                    <div class="flex justify-end space-x-4 mt-2">
-                        <a href="{{route("tareas.show", $tarea)}}" ><x-show-button/></a>
-                        <a href="{{route('tareas.edit', $tarea)}}"><x-update-button/></a>
+                    <div class="flex justify-between space-x-4 mt-2">
+                        <div>
+                        <button type="submit" class="flex items-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" />
+                            </svg>
+                        </button>
+                        </div>
                         <form action="{{route('tarea.destroy', $tarea)}}" method="post">
                             @csrf
                             @method('DELETE')
-                            <button data-modal-target="popup-modal" data-modal-toggle="popup-modal" type="button">
-                                <x-delete-buttom/>
-                            </button>
+                    <div class="flex flex-row space-x-2">
+                        <a href="{{route("tareas.show", $tarea)}}" ><x-show-button/></a>
+                        <a href="{{route('tareas.edit', $tarea)}}"><x-update-button/></a>
+                        <x-delete-modal id="popup-modal-{{ $tarea->id }}" description="¿Estas seguro de querer borrar esta tarea?"/>
+                    </div>
 
-                            <div id="popup-modal" tabindex="-1" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
-                                <div class="relative p-4 w-full max-w-md max-h-full">
-                                    <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-                                        <button type="button" class="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="popup-modal">
-                                            <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
-                                            </svg>
-                                            <span class="sr-only">Close modal</span>
-                                        </button>
-                                        <div class="p-4 md:p-5 text-center">
-                                            <svg class="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
-                                            </svg>
-                                            <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Estas seguro de querer borrar esta tarea?</h3>
-                                            <button data-modal-hide="popup-modal" type="submit" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center">
-                                               Eliminar
-                                            </button>
-                                            <button data-modal-hide="popup-modal" type="button" class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Cancelar</button>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
                         </form>
                     </div>
                 </div>
