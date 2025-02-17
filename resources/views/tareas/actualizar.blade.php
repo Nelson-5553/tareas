f102119bd9e6a7f61c4c736e63b88919fe73c866
Nelson-5553 <andresnelson2004@hotmail.com>
2025-02-16 21:41:58 -0500

    add icon update in categories

--- resources/views/tareas/actualizar.blade.php
+++ resources/views/tareas/actualizar.blade.php
@@ -2,33 +2,14 @@
 
 @section('content')
 
-<!--
-  This example requires some changes to your config:
-
-  ```
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/forms'),
-    ],
-  }
-  ```
--->
-<!--
-  This example requires updating your template:
-
-  ```
-  <html class="h-full bg-white">
-  <body class="h-full">
-  ```
--->
 <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
-    <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-      <img class="mx-auto h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company">
-      <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">To-DO</h2>
+
+    <div class="flex justify-center text-blue-500">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-36">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99" />
+      </svg>
     </div>
+    <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Actualizar la tarea</h2>
 
     <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
 
@@ -36,7 +17,7 @@
         @csrf
         @method('put')
         <div>
-          <label for="" class="block text-sm font-medium leading-6 text-gray-900">actualizar tarea la tarea</label>
+          <label for="" class="block text-sm font-medium leading-6 text-gray-900">Nombre de la tarea</label>
           <div class="mt-2">
             <input id="name" name="name" type="text" value="{{old('name',$tarea->name)}}" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
             @error('name')
@@ -62,13 +43,13 @@
         <div>
             <label for="" class="block text-sm font-medium leading-6 text-gray-900">Descripcion</label>
             <div class="mt-2">
-              <input id="name" name="descripcion" type="text-area" value="{{old('descripcion',$tarea->descripcion)}}" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+              <input id="name" name="descripcion" type="text" value="{{old('descripcion',$tarea->descripcion)}}" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
               @error('descripcion')
                 {{$message}}
               @enderror
             </div>
           </div>
-
+          <br>
         <div>
           <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Actualizar tarea</button>
         </div>
